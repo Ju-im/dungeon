9e10a8105415cf0f96a024674753699392dfdab5
Ju-im <joshua2.imarhiagbe@live.uwe.ac.uk>
2026-01-14 14:31:22 +0000

    update to weapon class sytem

--- src/Weapon.cpp
+++ src/Weapon.cpp
@@ -17,14 +17,45 @@ void Weapon::setAttackWidth(int width)
   attackWidth = width;
 }
 
-void Weapon::attack() {
-	
-	
+void Weapon::attack(Player& player, Grid& grid)
+{
+  char dir = 'u';
+  if (dir == 'u')
+  {
+    sf::Vector2i player_screen_pos = player.getScreenPosition(grid);
+    for (int i = 0; i < attackHeight; i++)
+    {
+      int y = player_screen_pos.y;
+      int x = player_screen_pos.x;
+      box[i].setPosition(x, y + i + 1);
+      box.pop_back();
+      //white white white, white black white
+    }
+  
+  
+  }
 
 
 
 }
 
+void Weapon::setNumberOfBoxes(int num) {
+  num_box = num;
+  box.resize(num_box);
+  for (int i = 0; i < box.size(); i++)
+  {
+    box[i].setFillColor(sf::Color::White);
+    box[i].setSize(sf::Vector2f(CELL_SIZE, CELL_SIZE));
+  
+  }
+
+}
+
+void Weapon::setEmptySpace(int num, sf::Vector2i pos) {
+
+
+
+}
 
 bool init() {
 
--- src/Weapon.h
+++ src/Weapon.h
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include "Grid.h"
+#include "Player.h"
 #include <iostream>
 #include <vector>
 class Weapon
@@ -11,11 +12,26 @@ class Weapon
 
   void setAttackHeight(int height);
   void setAttackWidth(int width);
-  void attack();
+  void attack(Player& player, Grid& grid);
+  void setNumberOfBoxes(int num);
+  void setEmptySpace(int num, sf::Vector2i pos);
+  sf::RectangleShape hitbox;
 
 
   private:
+  int CELL_SIZE    = 10;
   int attackHeight = 1;
    int attackWidth  = 1;
+  int num_box          = 1;
+   std::vector<sf::RectangleShape> box;
+   std::vector<std::vector<sf::RectangleShape>> boxes;
+};
+
+enum class WeaponType
+{
+
+
 
 };
+
+	
